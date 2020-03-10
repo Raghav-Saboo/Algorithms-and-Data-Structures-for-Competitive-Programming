91517cdbfa40888e7104cf656f6c82fa4e219d05
raghavbps <raghavs@flock.com>
2020-03-10 14:28:24 +0530

    binary lifting fix

--- Advanced Data structures/binary_lifting.cpp
+++ Advanced Data structures/binary_lifting.cpp
@@ -1,7 +1,7 @@
-// https://www.codechef.com/problems/CATS
+// https://www.spoj.com/problems/DISQUERY/
 #include <bits/stdc++.h>
 using namespace std;
-#define ll int
+#define ll long long int
 #define mp make_pair
 #define pb push_back
 #define pii pair<int,int>
@@ -13,101 +13,138 @@ using namespace std;
 #define rep(i,n)  for(i=0;i<n;i++)
 #define si(i)  scanf("%d",&i)
 #define ss(s)  scanf("%s",s)
-#define sl(i)  scanf("%d",&i)
+#define sl(i)  scanf("%lld",&i)
 #define pi(i)  printf("%d",i)
-#define pl(i)  printf("%d",i);
+#define pl(i)  printf("%lld",i);
 #define ps()   printf(" ");
 #define pn()   printf("\n");
 #define all(v)  v.begin(),v.end()
 #define vi vector <int>
 #define vl vector <ll>
-using namespace std;
-#define N 1000005
-vector <ll> adj[N];
-ll arr[N];
+#define N 100005
+#define LGN 20
+ll dp[N][LGN][3];
+vector <pll> adj[N];
 ll vis[N];
-ll dp[N][25];
-ll cat[N];
-ll par[N];
-ll lev[N];
-void dfs(ll u,ll pr,ll lv)
+ll lev[N],par[N];
+ll pared[N];
+void dfs(ll u,ll p,ll lv,ll ew)
 {
-	vis[u]=1;
-	par[u]=pr;
-	lev[u]=lv;
-	for(ll v:adj[u])
-	{
-		if(!vis[v])
-		{
-			dfs(v,u,lv+1);
-		}
-	}
+  ll i,v,n=adj[u].size(),w;
+  vis[u]=1;
+  lev[u]=lv;
+  par[u]=p;
+  pared[u]=ew;
+  rep(i,n)
+  {
+    v=adj[u][i].fi;
+    w=adj[u][i].se;
+    if(!vis[v])
+    {
+      dfs(v,u,lv+1,w);
+    }
+  }
 }
-void pre_lca(ll n)
+void precomp(ll n)
 {
-	ll i,j;
-	fu(i,1,n)
-	{
-		dp[i][0]=par[i];
-	}
-	fu(j,1,20)
-	{
-		fu(i,1,n)
-		{
-			if(dp[i][j-1])
-			{
-				dp[i][j]=dp[dp[i][j-1]][j-1];
-			}
-		}
-	}
+  ll i,j;
+  for(i=0;i<=n;i++)
+  {
+    for(j=0;(1<<j)<=n;j++)
+    {
+      dp[i][j][0]=-1;
+      dp[i][j][1]=1e8; // min
+      dp[i][j][2]=-1; // max
+    }
+  }
+  for(i=1;i<=n;i++)
+  {
+    dp[i][0][0]=par[i];
+    dp[i][0][1]=dp[i][0][2]=pared[i];
+  }
+  for(j=1;(1<<j)<=n;j++)
+  {
+    for(i=1;i<=n;i++)
+    {
+      if(dp[i][j-1][0]!=-1)
+      {
+        dp[i][j][0]=dp[dp[i][j-1][0]][j-1][0];
+        dp[i][j][1]=min(dp[dp[i][j-1][0]][j-1][1],dp[i][j-1][1]);
+        dp[i][j][2]=max(dp[dp[i][j-1][0]][j-1][2],dp[i][j-1][2]);
+      }
+    }
+  }
 }
-ll cal(ll u)
+ll lca(ll u,ll v)
 {
-	ll ans=0;
-	ll i,v=u;
-	if(cat[u])return ans;
-	fd(i,20,0)
-	{
-		if(dp[v][i]&&!cat[dp[v][i]])
-		{
-			v=dp[v][i];
-		}
-	}
-	cat[v]=1;
-	ans=lev[u]-lev[v]+1;
-	return ans;
+  // cout<<u<<" "<<v<<endl;
+  ll tmp,p,q,logi,i;
+  p=u;q=v;
+  if(lev[p]<lev[q])
+  {
+    tmp=p;
+    p=q;
+    q=tmp;
+  }
+  for(logi=1;(1<<logi)<=lev[p];logi++);
+  logi--;
+  // cout<<logi<<endl;
+  for(i=logi;i>=0;i--)
+  {
+    if(lev[p]-(1<<i)>=lev[q])
+    {
+      p=dp[p][i][0];
+    }
+  }
+  // cout<<p<<endl;
+  if(p==q)return p;
+  for(i=logi;i>=0;i--)
+  {
+    if(dp[p][i][0]!=-1&&dp[p][i][0]!=dp[q][i][0])
+    {
+      // cout<<dp[p][i]<<" "<<dp[q][i]<<endl;
+      p=dp[p][i][0];
+      q=dp[q][i][0];
+    }
+  }
+  return par[p];
+}
+void cal(ll u,ll lca,ll &mn,ll &mx)
+{
+  ll x=lev[u]-lev[lca],i;
+  fd(i,20,0)
+  {
+    if(x&(1<<i))
+    {
+      mn=min(mn,dp[u][i][1]);
+      mx=max(mx,dp[u][i][2]);
+      u=dp[u][i][0];
+    }
+  }
 }
 int main()
 {
-	ll n,i,j,x,u,v,ans,root=0;
-	sl(n);
-	fu(i,1,n)
-	{
-		sl(x);
-		if(x==0)
-		{
-			root=i;
-		}
-		else
-		{
-			adj[x].pb(i);
-		}
-	}
-	fu(i,1,n)
-	{
-		sl(arr[i]);
-	}
-	dfs(root,0,0);
-	pre_lca(n);
-	fu(i,1,n)
-	{
-		vis[i]=0;
-	}
-	fu(i,1,n)
-	{
-		ans=cal(arr[i]);
-		pl(ans);pn();
-		u=arr[i];
-	}
-	return 0;
+  ll t,n,q,i,j,x,y,ans,u,v,tc,w,mn,mx;
+  sl(n);
+  fu(i,1,n-1)
+  {
+    sl(u);sl(v);sl(w);
+    adj[u].pb(mp(v,w));
+    adj[v].pb(mp(u,w));
+  }
+  dfs(1,-1,1,-1);
+  precomp(n);
+  sl(q);
+  while(q--)
+  {
+    sl(u);sl(v);
+    ans=lca(u,v);
+    mn=1e8;
+    mx=0;
+    cal(u,ans,mn,mx);
+    cal(v,ans,mn,mx);
+    if(mn==1e8)mn=0;
+    pl(mn);ps();pl(mx);pn();
+  }
+  return 0;
 }
---
+++ Heaps,priority queue,searching and sorting/set_custom_compare.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mp make_pair
+#define pb push_back
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define fi first
+#define se second
+#define fu(i,a,n) for(i=a;i<=n;i++)
+#define fd(i,a,n) for(i=a;i>=n;i--)
+#define rep(i,n)  for(i=0;i<n;i++)
+#define si(i)  scanf("%d",&i)
+#define ss(s)  scanf("%s",s)
+#define sl(i)  scanf("%lld",&i)
+#define pi(i)  printf("%d",i)
+#define pl(i)  printf("%lld",i);
+#define ps()   printf(" ");
+#define pn()   printf("\n");
+#define all(v)  v.begin(),v.end()
+#define vi vector <int>
+#define vl vector <ll>
+#define N 1000005
+// always remember that strict ordering is required so never use a<=b instead use a<b or b>a
+struct cmp
+{
+  bool operator()(int a,int b)
+  {
+    return a>b; // to sort in descending order, use a<b for ascending order
+  }
+};
+int main()
+{
+  set <ll,cmp> st;
+  ll i,n,x;
+  sl(n);
+  fu(i,1,n)
+  {
+    sl(x);
+    st.insert(x);
+  }
+  for(auto it=st.begin();it!=st.end();it++)
+  {
+    cout<<*it<<endl;
+  }
+}
